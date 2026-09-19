7bff88ef2130a9f8f53420c97ea3c70288c12ce5
berkankurtaran <berkankurtaran5454@gmail.com>
2026-09-19 19:42:50 +0300

    Revert CSS crop and use processed transparent logo

--- remove_bg.py
+++ remove_bg.py
@@ -1,29 +1,27 @@
-from PIL import Image, ImageDraw
+from PIL import Image
 
-def make_transparent():
-    # Open the uploaded image
+def remove_background():
     img_path = r"C:\Users\berka\.gemini\antigravity-ide\brain\a529fa63-4b00-4364-aa1e-6a28446e3df8\.user_uploaded\media_1789835439329.jpg"
-    try:
-        img = Image.open(img_path).convert("RGBA")
-    except Exception as e:
-        print("Could not open image:", e)
-        return
-
+    img = Image.open(img_path).convert("RGBA")
+    
     datas = img.getdata()
     newData = []
     
-    # Remove black background (threshold < 20)
     for item in datas:
-        if item[0] < 25 and item[1] < 25 and item[2] < 25:
-            newData.append((0, 0, 0, 0)) # Transparent
+        r, g, b, a = item
+        # If pixel is dark (black background or dark green glow)
+        if r < 45 and b < 45 and g < 80:
+            # Calculate brightness to determine transparency
+            brightness = (r + g + b) / 3.0
+            # Map brightness to alpha (0 brightness = 0 alpha, 40 brightness = 255 alpha)
+            new_a = int(max(0, min(255, (brightness / 35.0) * 255)))
+            newData.append((r, g, b, new_a))
         else:
-            newData.append(item)
-
+            newData.append((r, g, b, 255))
+            
     img.putdata(newData)
-    
-    # Save as PNG
     img.save("assets/images/logo.png", "PNG")
-    print("Created transparent logo.png successfully.")
+    print("Smooth background removal complete.")
 
 if __name__ == '__main__':
-    make_transparent()
+    remove_background()
